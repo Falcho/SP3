ca4633b2ff0cac79ab35c3a998c8afef3cac6f79
MojoOno <Ffranck0406@gmail.com>
2024-04-17 14:55:45 +0200

    ChillFlix runDialog v1

--- src/ChillFlix.java
+++ src/ChillFlix.java
@@ -1,2 +1,30 @@
+import util.TextUI;
+
+import java.util.ArrayList;
+
 public class ChillFlix {
+    TextUI ui = new TextUI();
+
+    public void runDialog() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Opret bruger");
+        list.add("Login");
+        list.add("Luk Chillflix");
+        ui.displayMsg("Login menu");
+        boolean proceed = true;
+        while (proceed) {
+            int choice = ui.promptChoice(list, "Vælg en handling");
+            switch (choice) {
+                case 1:
+                    ui.displayMsg("Opret bruger");
+                    break;
+                case 2:
+                    ui.displayMsg("Login");
+                    break;
+                case 3:
+                    proceed = false;
+                    break;
+            }
+        }
+    }
 }
