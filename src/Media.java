b4136c7018a94a222663368934fafb3deeaecfe7
MojoOno <Ffranck0406@gmail.com>
2024-04-18 11:32:24 +0200

    Added interface Media

---
+++ src/Media.java
@@ -0,0 +1,10 @@
+public interface Media {
+    public void play();
+    public void stop();
+    public void pause();
+    public void restart();
+    public void skipIntro();
+    public void playPrevious();
+    public void playNext();
+
+}
