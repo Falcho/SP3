0806a307c873fcaa3864359c722327435dd2ddd5
MojoOno <Ffranck0406@gmail.com>
2024-04-18 15:38:11 +0200

    added methods for textUI sprint 1

--- src/util/TextUI.java
+++ src/util/TextUI.java
@@ -1,12 +1,70 @@
 package util;
 
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextUI {
 
     private Scanner scan = new Scanner(System.in);
 
+    public void displayMsg(String msg) {
+        System.out.println(msg);
+    }
+
+    public void displayList(List<String> list) {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public void displayMedia(List<Media> mediaList, int start) {
+         if (mediaList.size() > start && mediaList.size() <= 10) {
+            for (int i = start; i < mediaList.size(); i++) {
+                System.out.println(mediaList.get(i));
+            }
+        }
+    }
+
+    public String promptText(String s) {
+        displayMsg(s);
+        String input = scan.nextLine();
+        return input;
+    }
+
+    public int promptNumeric(String s, int maxValue) {
+        displayMsg(s);
+        if (scan.hasNextInt()) {
+            String input = scan.next();
+            int parsedInt = Integer.parseInt(input);
+            if (parsedInt > 0 && parsedInt <= maxValue) {
+                return parsedInt;
+            }
+        }
+        return promptNumeric(s, maxValue);
+    }
+
+    public boolean promptBinary(String msg, String accept, String reject) {
+        String input = promptText(msg);
+        if (input.equalsIgnoreCase(accept)) {
+            return true;
+        } else if (input.equalsIgnoreCase(reject)) {
+            return false;
+        } else {
+            return promptBinary("Ugyldigt input - prøv igen.\n" + msg, accept, reject);
+        }
+    }
+
+    public int promptChoice(List<String> inputList, String s) {
+        displayList(inputList);
+        int maxValue = inputList.size();
+        int input = promptNumeric(s, maxValue);
+        return input;
+    }
+
+
+}
 
 
 
@@ -80,4 +138,3 @@ public class TextUI {
         System.out.println("***************\n");
     }
     */
-}
