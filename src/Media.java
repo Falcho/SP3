d0871cf533360bf067a5bed9c39a82f39ca99de5
AndyTheDragon <158449635+AndyTheDragon@users.noreply.github.com>
2024-04-18 15:37:10 +0200

    Added constructor

--- src/Media.java
+++ src/Media.java
@@ -4,6 +4,13 @@ abstract public class Media implements IMedia{
     private String genre;
     private float rating;
 
+    Media(String title, int releaseYear, String genre, float rating) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
     public String getTitle() {
         return title;
     }
