8e4fb3101ac9d34074c9154664ae5495622ec50d
AndyTheDragon <158449635+AndyTheDragon@users.noreply.github.com>
2024-04-22 16:41:56 +0200

    OPrettet episode klassen

---
+++ src/Episode.java
@@ -0,0 +1,5 @@
+public class Episode extends Movie{
+    public Episode(String title, int releaseYear, String genre, float rating, int duration) {
+        super(title, releaseYear, genre, rating, duration);
+    }
+}
